316162115ba3d336aa14114ef3d67357eab7aab1
andrewetobin <andrewetobin@gmail.com>
2018-10-30 11:13:48 -0600

    start refactor of big facade method

---
+++ app/controllers/api/v1/games/plays_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Games::PlaysController < ApplicationController
+
+  def create
+    play = Play.create(game_id: params[:game_id], user_id: params[:user_id], word: params[:word])
+    render status: 201, json: Game.find(params[:game_id])
+  end
+end
