29001d892e818c126377f68f3af39ec54c7fbc71
andrewetobin <andrewetobin@gmail.com>
2018-10-30 09:38:59 -0600

    starts view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
 
   def index
+    @search_facade = SearchFacade.new(params[:q])
 
   end
 end
---
+++ app/models/search_facade.rb
@@ -0,0 +1,14 @@
+class SearchFacade
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def examples
+    
+  end
+
+
+
+end
